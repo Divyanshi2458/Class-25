96965125c16d0bcd0ee41794b583f70a0c1760bf
Divyanshi2458 <70753678+Divyanshi2458@users.noreply.github.com>
2020-10-18 20:28:19 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,34 @@
+class ground
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true
+		
+
+			
+			}
+		this.x=x;
+		this.y=y;
+        this.w=w
+        this.h=h
+		this.body=Bodies.rectangle(this.x, this.y, this.w, this.height,options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var groundpos=this.body.position;		
+
+			push()
+			translate(groundpos.x, groundpos.y);
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill(255,0,255)
+			rect(0,0,this.w, this.h);
+			pop()
+			
+	}
+
+}
